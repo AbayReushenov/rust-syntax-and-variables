3636a0e911cd136c958331f724db7f723eea199f
AbayReushenov <abay.reushenov@gmail.com>
2025-09-03 16:50:16 +0300

    Shadowing -  как работают переменные

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
 fn main() {
-    let x = 5;
+    let spaces = "   ";
+    let spaces = spaces.len();
 
-    let x = x + 1;
-    
-    // shadowing (переопределение переменных)
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
+    println!("The value of spaces is: {spaces}");
 }
-
-
-// $ cargo run
-//    Compiling variables v0.1.0 (/home/aaaaa/rust/variables)
-//     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.76s
-//      Running `target/debug/variables`
-// The value of x in the inner scope is: 12
-// The value of x is: 6
