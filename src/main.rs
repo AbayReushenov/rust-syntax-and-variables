e12971a6f644033753b20bd5c0acaf860ccb4abf
AbayReushenov <abay.reushenov@gmail.com>
2025-09-03 17:12:54 +0300

    Functions in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,10 @@
-use std::io;
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("Hello, world!");
 
-    println!("Please enter an array index.");
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+    another_function();
 }
 
-// $ cargo run
-//     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.04s
-//      Running `target/debug/variables`
-// Please enter an array index.
-// 999
-
-// thread 'main' panicked at src/main.rs:19:19:
-// index out of bounds: the len is 5 but the index is 999
-// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+fn another_function() {
+    println!("Another function.");
+}
