6f02487f1fee75e2a0820ef611f47f4a0cfe22da
AbayReushenov <abay.reushenov@gmail.com>
2025-09-03 16:55:49 +0300

    Tulip - кортеж

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 fn main() {
-    let spaces = "   ";
-    let spaces = spaces.len();
+    // tuple - кортеж;
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
 
-    println!("The value of spaces is: {spaces}");
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+    println!("The value of z is: {z}");
+    println!("The value of x is: {x}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    // tuple indexing by position
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("The value of five_hundred is: {five_hundred}");
+    println!("The value of six_point_four is: {six_point_four}");
+    println!("The value of one is: {one}");
 }
+// The value of y is: 6.4
+// The value of z is: 1
+// The value of x is: 500
+// The value of five_hundred is: 500
+// The value of six_point_four is: 6.4
+// The value of one is: 1
