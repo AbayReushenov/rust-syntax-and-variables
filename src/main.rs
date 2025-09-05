977a23e0884f100f90ccbf8154c68f6664b46d5b
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 11:33:20 +0300

    Управляющие конструкции. IF

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,60 @@
-// Комментарии
-// Hello, world.
-
-
-// Итак, мы делаем что-то сложное, настолько длинное, что нам нужно
-// несколько строк комментариев, чтобы сделать это! Ух! Надеюсь, этот комментарий
-// объясняет, что происходит.
-
+// Управляющие конструкции
+// Возможности запуска некоторого кода в зависимости от некоторого условия, и циклического выполнения некоторого кода,
+// являются базовыми элементами в большинстве языков программирования. Наиболее распространёнными конструкциями,
+// позволяющими управлять потоком выполнения кода Rust, являются выражения if и циклы.
 fn main() {
-    // I'm feeling lucky today. Так лучше!
-    let lucky_number = 7; // I'm feeling lucky today
+    let number = 3;
 
-    println!("Комментарии: Lucky_mumber is {}", lucky_number)
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // Ошибка, Rust ожидал тип bool
+
+    // let number_test_two = 3;
+
+    // if number_test_two {
+    //     println!("number was three");
+    // }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let condition = true;
+    let number_condition = if condition { 5 } else { 6 };
+
+    println!("The value of number_condition is: {number_condition}");
+
+    // let condition_2 = true;
+
+    // let number_2 = if condition_2 { 5 } else { "six" };
+
+    // Compiling variables v0.1.0 (/home/aaaaa/rust/variables)
+    // error[E0308]: `if` and `else` have incompatible types
+    //   --> src/main.rs:33:48
+    //    |
+    // 33 |     let number_2 = if condition_2 { 5 } else { "six" };
+    //    |                                     -          ^^^^^ expected integer, found `&str`
+    //    |                                     |
+    //    |                                     expected because of this
+
+    // For more information about this error, try `rustc --explain E0308`.
+    // println!("The value of number is: {number_2}");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
 }
+// condition was true
+// number was something other than zero
