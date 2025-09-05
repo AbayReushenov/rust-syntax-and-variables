33d04b90ed1dab9fe8f29cdec73cfb01e1e95b95
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 15:08:59 +0300

    Передача переменной в функцию приведёт к перемещению или копированию, как и присваивание

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,23 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let s = String::from("hello");  // s comes into scope
 
-    println!("s1 = {s1}, s2 = {s2}");
-    // s1 = hello, s2 = hello
-    let x = 5;
-    let y = x;
+    takes_ownership(s);             // s's value moves into the function...
+                                    // ... and so is no longer valid here
 
-    println!("x = {x}, y = {y}");
+    let x = 5;                      // x comes into scope
 
-    // s1 = hello, s2 = hello
-    // x = 5, y = 5
+    makes_copy(x);                  // Because i32 implements the Copy trait,
+                                    // x does NOT move into the function,
+                                    // so it's okay to use x afterward.
 
-    let s01 = String::from("hello");
-    let s02 = s01;
+} // Here, x goes out of scope, then s. However, because s's value was moved,
+  // nothing special happens.
 
-    // println!("{s01}, world!"); // borrow of moved
-    println!("{s02}, world!");
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
 
-    let s001 = "hello";
-    let s002 = s001;
-
-    println!("{s001}, world!");
-    println!("{s002}, world!");
-}
-
-// s1 = hello, s2 = hello
-// x = 5, y = 5
-// hello, world!
-// hello, world!
-// hello, world!
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{some_integer}");
+} // Here, some_integer goes out of scope. Nothing special happens.
