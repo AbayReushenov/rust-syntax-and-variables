effc3751b62d321c5654bcdd794a22372db736e6
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 16:15:23 +0300

    Fixed. Решением будет вернуть непосредственно String:

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let string = no_dangle();
+
+    println!("Test {}", string)
 }
 
-fn dangle() -> &String { // dangle returns a reference to a String
+fn no_dangle() -> String {
+    let s = String::from("hello");
 
-    let s = String::from("hello"); // s is a new String
+    s
+}
 
-    &s // we return a reference to the String, s
-} // Here, s goes out of scope and is dropped, so its memory goes away.
-  // Danger!
-  // Поскольку s создаётся внутри dangle, когда код dangle будет завершён, s будет освобождена.
-  // Но мы попытались вернуть ссылку на неё. Это означает, что эта ссылка будет указывать на недопустимую String.
-  // Это нехорошо! Rust не позволит нам сделать это.
+// Test hello
