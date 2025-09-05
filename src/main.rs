183900d479246023d995967fa10bb31b16d294d8
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 13:06:37 +0300

    Value borrowed after move

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
 fn main() {
-    {                      // s is not valid here, since it's not yet declared
-        let s = "hello";   // s is valid from this point forward
+    let s1 = String::from("hello");
+    let s2 = s1;
 
-        // do stuff with s
-        println!("s {}", s)
-    }
-    // this scope is now over, and s is no longer valid
-
-    let s = "hello 333";
-    println!("s {}", s);
-
-    // Оператор "Двойное двоеточие" :: позволяет использовать пространство имён
-    // данной конкретной функции from с типом String
-    let mut s3 = String::from("hello ");
-
-    println!("S#3 {}", s3);
-
-    s3.push_str(", world!"); // push_str() appends a literal to a Stri
-
-    println!("S#3 {}", s3);
-
-    // s hello
-    // s hello 333
-    // S#3 hello
-    // S#3 hello , world!
+    println!("{s1}, world!");
 }
+
+//   |              -- value moved here
+// 4 |
+// 5 |     println!("{s1}, world!");
+//   |                ^^ value borrowed here after move
