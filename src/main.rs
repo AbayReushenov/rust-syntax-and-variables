4dfc850286c198851c77df469b25b22efdc99803
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 16:12:35 +0300

    Error borrowing

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,6 @@ fn dangle() -> &String { // dangle returns a reference to a String
     &s // we return a reference to the String, s
 } // Here, s goes out of scope and is dropped, so its memory goes away.
   // Danger!
-  // Поскольку s создаётся внутри dangle, когда код dangle будет завершён, s будет освобождена. Но мы попытались вернуть ссылку на неё. Это означает, что эта ссылка будет указывать на недопустимую String. Это нехорошо! Rust не позволит нам сделать это.
+  // Поскольку s создаётся внутри dangle, когда код dangle будет завершён, s будет освобождена.
+  // Но мы попытались вернуть ссылку на неё. Это означает, что эта ссылка будет указывать на недопустимую String.
+  // Это нехорошо! Rust не позволит нам сделать это.
