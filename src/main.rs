b5480b4201864828cdd59db84607663865ea9eaf
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 16:11:01 +0300

    Error borrowing: Поскольку s создаётся внутри dangle, когда код dangle будет завершён, s будет освобождена. Но мы попытались вернуть ссылку на неё. Это означает, что эта ссылка будет указывать на недопустимую String. Это нехорошо! Rust не позволит нам сделать это.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 fn main() {
     let reference_to_nothing = dangle();
-
-    println!("Test {}", reference_to_nothing)
 }
 
+fn dangle() -> &String { // dangle returns a reference to a String
 
-// this function's return type contains a borrowed value, but there is no value for it to be borrowed from
+    let s = String::from("hello"); // s is a new String
 
-fn dangle() -> &String {
-    let s = String::from("hello");
-
-    &s
-}
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope and is dropped, so its memory goes away.
+  // Danger!
+  // Поскольку s создаётся внутри dangle, когда код dangle будет завершён, s будет освобождена. Но мы попытались вернуть ссылку на неё. Это означает, что эта ссылка будет указывать на недопустимую String. Это нехорошо! Rust не позволит нам сделать это.
