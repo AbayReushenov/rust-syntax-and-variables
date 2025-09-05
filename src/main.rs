dbfcf66984120ab45aa1e99957452c54ff1e7a0a
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 11:38:45 +0300

    Метки цикла

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,41 @@
-// Управляющие конструкции
-// Возможности запуска некоторого кода в зависимости от некоторого условия, и циклического выполнения некоторого кода,
-// являются базовыми элементами в большинстве языков программирования. Наиболее распространёнными конструкциями,
-// позволяющими управлять потоком выполнения кода Rust, являются выражения if и циклы.
+// Метки циклов для устранения неоднозначности между несколькими циклами
+// Если у вас есть циклы внутри циклов, break и continue применяются к самому внутреннему циклу в этой цепочке.
+// При желании вы можете создать метку цикла, которую вы затем сможете использовать с break или continue для указания,
+// что эти ключевые слова применяются к помеченному циклу, а не к самому внутреннему циклу.
+// Метки цикла должны начинаться с одинарной кавычки.
+// Вот пример с двумя вложенными циклами:
+
+
 fn main() {
-    let number = 3;
+    let mut count = 0;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
-    }
+    // Метки цикла должны начинаться с одинарной кавычки.
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
 
-    // Ошибка, Rust ожидал тип bool
-
-    // let number_test_two = 3;
-
-    // if number_test_two {
-    //     println!("number was three");
-    // }
-
-    if number != 0 {
-        println!("number was something other than zero");
-    }
-
-    let condition = true;
-    let number_condition = if condition { 5 } else { 6 };
-
-    println!("The value of number_condition is: {number_condition}");
-
-    // let condition_2 = true;
-
-    // let number_2 = if condition_2 { 5 } else { "six" };
-
-    // Compiling variables v0.1.0 (/home/aaaaa/rust/variables)
-    // error[E0308]: `if` and `else` have incompatible types
-    //   --> src/main.rs:33:48
-    //    |
-    // 33 |     let number_2 = if condition_2 { 5 } else { "six" };
-    //    |                                     -          ^^^^^ expected integer, found `&str`
-    //    |                                     |
-    //    |                                     expected because of this
-
-    // For more information about this error, try `rustc --explain E0308`.
-    // println!("The value of number is: {number_2}");
-
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
         }
-    };
 
-    println!("The result is {result}");
+        count += 1;
+    }
+    
+    println!("End count = {count}");
 }
-// condition was true
-// number was something other than zero
+// count = 0
+// remaining = 10
+// remaining = 9
+// count = 1
+// remaining = 10
+// remaining = 9
+// count = 2
+// remaining = 10
+// End count = 2
