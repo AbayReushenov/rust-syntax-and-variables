f4fd46cb81c7fe29a018e6ef0cdb7f99f83d13e0
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 15:07:31 +0300

    Взаимодействие переменных и данных с помощью клонирования.Стековые данные: копирование

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 fn main() {
     let s1 = String::from("hello");
-    let s2 = s1;
+    let s2 = s1.clone();
 
-    println!("{s1}, world!");
+    println!("s1 = {s1}, s2 = {s2}");
+    // s1 = hello, s2 = hello
+    let x = 5;
+    let y = x;
+
+    println!("x = {x}, y = {y}");
+
+    // s1 = hello, s2 = hello
+    // x = 5, y = 5
+
+    let s01 = String::from("hello");
+    let s02 = s01;
+
+    // println!("{s01}, world!"); // borrow of moved
+    println!("{s02}, world!");
+
+    let s001 = "hello";
+    let s002 = s001;
+
+    println!("{s001}, world!");
+    println!("{s002}, world!");
 }
 
-//   |              -- value moved here
-// 4 |
-// 5 |     println!("{s1}, world!");
-//   |                ^^ value borrowed here after move
+// s1 = hello, s2 = hello
+// x = 5, y = 5
+// hello, world!
+// hello, world!
+// hello, world!
