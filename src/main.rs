fca55f5d33ac56d166013ac800dcc8b0b2e93551
AbayReushenov <abay.reushenov@gmail.com>
2025-09-05 17:41:59 +0300

    Примеры срезов

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,38 @@ fn first_word(s:  &str) -> &str {
 
     &s[..]
 }
-
 fn main() {
-    let s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s);
+    // `first_word` works on slices of `String`s, whether partial or whole.
+    let word = first_word(&my_string[0..6]);
 
-    // s.clear(); // error!
+    println!(" ТЕСТ {}", word);
 
-    println!("the first word is: {word}");
+    let word = first_word(&my_string[..]);
+
+    println!(" ТЕСТ 2 {}", word);// `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s.
+    let word = first_word(&my_string);
+    println!(" ТЕСТ 3 {}", word);
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or
+    // whole.
+    let word = first_word(&my_string_literal[0..6]);
+    println!(" ТЕСТ 4 {}", word);
+    let word = first_word(&my_string_literal[..]);
+    println!(" ТЕСТ 5 {}", word);
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+    println!(" ТЕСТ 6 {}", word);
 }
 
-// the first word is: hello
+
+// ТЕСТ hello
+// ТЕСТ 2 hello
+// ТЕСТ 3 hello
+// ТЕСТ 4 hello
+// ТЕСТ 5 hello
+// ТЕСТ 6 hello
